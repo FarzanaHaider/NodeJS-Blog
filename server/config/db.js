3b416df22429d36a25912eb34c9750751521bcac
FarzanaHaider <farzanahaider1425@gmail.com>
2025-04-04 00:30:49 +0600

    Routing Post

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,13 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const connectDB = async () => {
 
     try{
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        // const conn = await mongoose.connect(process.env.MONGODB_URI);  It was shown but somehow not working
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            tls: true, // Ensures TLS is enabled
+            tlsAllowInvalidCertificates: true, // Bypass certificate validation issues
+        });
         console.log(`Database Connected: ${conn.connection.host}`);
     }catch(error) {
         console.log(error);
